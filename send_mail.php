c10db2d7bb58c58081b1dd1d28808c935192c332
MansiTandel <mtandel@scu.edu>
2024-07-10 14:39:50 -0700

    Changes to team and contact

---
+++ send_mail.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "contact@goodbuys.info";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Email successfully sent!";
+    } else {
+        echo "Failed to send the email, please try again.";
+    }
+}
+?>
